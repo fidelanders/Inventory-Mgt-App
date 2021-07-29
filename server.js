74d3fbdb39d8d178b5fa8f31dcda9490e364f386
fidelanders <85418769+fidelanders@users.noreply.github.com>
2021-07-29 11:55:44 +0100

    inventory-app deployed

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+
+const userRoute = require('./Route/userRoute')
+const productRoute = require('./Route/productRoute')
+const app = express();
+
+PORT = process.env.PORT;
+
+//mounting users routes
+app.use('/users', userRoute);
+app.use('/products', productRoute);
+//connecting and listening to server 
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
