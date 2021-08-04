b91af12709f9e47d9c9391cd739d29d4e8a0bf32
fidelanders <85418769+fidelanders@users.noreply.github.com>
2021-08-04 11:06:11 +0100

    update ngrok deployed

--- server.js
+++ server.js
@@ -7,10 +7,27 @@ const app = express();
 
 PORT = process.env.PORT;
 
+
+//using ngrok
+const ngrok = require('ngrok');
+(async function() {
+  const url = await ngrok.connect(PORT);
+  console.log(url)
+})();
+
+app.get("/", (req, res)=>{
+res.json("Welcome to Inventory Mgt API")
+
+})
+
 //mounting users routes
 app.use('/users', userRoute);
 app.use('/products', productRoute);
-//connecting and listening to server 
+
+
+// connecting and listening to server 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+
