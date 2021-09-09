66c6887d840cff4a18df510e861e35b5b5e638d6
Fidel Dash <85418769+fidelanders@users.noreply.github.com>
2021-09-09 15:26:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,8 @@ const app = express();
 PORT = process.env.PORT;
 
 
-// //using ngrok
-// const ngrok = require('ngrok');
-// (async function() {
-//   const url = await ngrok.connect(PORT);
-//   console.log(url)
-// })();
-
-
 app.get("/", (req, res)=>{
-res.json("Welcome to EMERGENCY REPORT APP API")
+res.send("Welcome to EMERGENCY REPORT APP API")
 })
 
 //mounting users routes
